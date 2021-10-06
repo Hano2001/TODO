c2ef66d00163a1e94ff66109eacc93648863e49c
Harald <Harald.Norlin@yh.nackademin.se>
2021-10-07 01:20:29 +0200

    Cors,Database,Clean code

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
 
 function App() {
+  let kittens = fetch("http://localhost:3000/kittens").then((res) =>
+    res.json()
+  );
+
+  async function window() {
+    let test = await kittens;
+    let testArray = [];
+
+    test.forEach((element) => {
+      testArray.push(element);
+    });
+    //let test2 = JSON.stringify(test[1]);
+    //console.log(await Object.keys(kittens).length);
+    alert(testArray[1].name);
+  }
   return (
     <div className="App">
       <h1>TODO START</h1>
+      <button onClick={window}>KLICKA</button>
     </div>
   );
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const kittens = [
+  {
+    id: 1,
+    name: "Lucy",
+    gender: "female",
+    birthDate: "2016-08-17",
+    color: "gray",
+  },
+  {
+    id: 2,
+    name: "Sasha",
+    gender: "female",
+    birthDate: "2003-05-27",
+    color: "white",
+  },
+];
+
+router.get("/", function (req, res, next) {
+  res.json({ name: "Harald" });
 });
+
+router.get("/kittens", function (req, res, next) {
+  res.json(kittens);
+});
+
+router.get("/api/todo", (req, res, next) => {});
+
+router.post("/api/todo", (req, res, next) => {});
+
+router.get("/api/todo/:id", (req, res, next) => {});
+
+router.post("/api/todo/:id", (req, res, next) => {});
+
+router.delete("/api/todo/:id", (req, res, next) => {});
 
 module.exports = router;
