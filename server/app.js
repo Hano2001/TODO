9e8d42fdb967f4b0e38ee32cdc49cc4f3ee517b6
Harald <Harald.Norlin@yh.nackademin.se>
2021-11-07 13:03:29 +0100

    env and cors change

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 var express = require("express");
 const cors = require("cors");
 
 var app = express();
 const ListRoute = require("./routes/ListRoute");
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT,
+    methods: "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/lists", ListRoute);
-
 
 module.exports = app;
